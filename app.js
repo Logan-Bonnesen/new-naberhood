2daf696641d396b4efc335c2b1f68b43704a607a
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2023-05-03 16:07:01 -0700

    adding get route for index.html

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"))
+})
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
